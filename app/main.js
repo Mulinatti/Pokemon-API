f489096859035c79655f83271bd9e76c9172c7f0
Gabriel Mulinari <gabrielmulinari2002@gmail.com>
2023-03-10 01:25:53 -0300

    find the pokemons

--- app/main.js
+++ app/main.js
@@ -6,10 +6,12 @@ async function getPokemonData(id) {
         const pokemonInfo = {
             name: pokemonData.name,
             id: pokemonData.id,
-            picture: pokemonData.sprites.front_default
+            picture: pokemonData.sprites.front_default,
+            types: pokemonData.types,
+            color: pokemonData.color
         }
 
-        console.log(pokemonInfo.picture);
+        console.log(pokemonInfo.color);
         return pokemonInfo
     }
     catch(erro) {
@@ -18,25 +20,35 @@ async function getPokemonData(id) {
     }
 }
 
+const input = document.getElementById("pokemonInput");
+const btn = document.getElementById("btn");
 
-const main = Promise.resolve(getPokemonData("1"));
-const mainTag = document.querySelector("main");
+btn.addEventListener("click", () => {
 
-main.then((pokemon) =>{
+    const main = Promise.resolve(getPokemonData(input.value));
+    const sectionTag = document.querySelector("section");
 
-    mainTag.innerHTML += 
-    `
-    <main class="font-nunito flex items-center justify-center h-screen">
-        <div class=" text-stone-50 w-44 p-3 bg-lime-600 flex flex-col items-center rounded-lg shadow-md">
+    main.then((pokemon) =>{
+    
+        sectionTag.innerHTML = 
+        `
+        <div class="text-stone-50 w-44 p-3 bg-lime-600 flex flex-col items-center rounded-lg shadow-md">
             <img class="w-36 bg-lime-700 rounded-lg" src="${pokemon.picture}">
             <div class="flex flex-col items-center justify-around h-14">
                 <p class="font-bold text-lg capitalize">${pokemon.name}</p>
                 <p class="font-extralight">${pokemon.id}</p>
             </div>
-            <div class="w-full flex items-center justify-between">
-                <p class="p-1 w-type bg-green-800 text-center rounded-xl">Grass</p>
-                <p class="p-1 w-type bg-lime-700 text-center rounded-xl">Bug</p>
+            <div class="w-full flex items-center justify-around type-box">
             </div>
         </div>
-    `
+        `
+        pokemonType = pokemon.types;
+        pokemonType.forEach(type => {
+            const tipagem = document.querySelector(".type-box");
+            tipagem.innerHTML += 
+            `
+            <p class="p-1 w-type bg-green-800 text-center rounded-xl capitalize">${type.type.name}</p>
+            `
+        })
+    });
 });
